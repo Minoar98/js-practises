8f96bb654a69cbac4d2d8550d850e1a77bdc055e
Minoar98 <minoarhasan@gmail.com>
2024-08-22 20:13:06 +0600

    practises added

--- event.js
+++ event.js
@@ -1,3 +1,9 @@
-let x = "John";
-let y = new String("John");
-console.log(x===y);
+// let x = "John";
+// let y = new String("John");
+// console.log(x===y);
+
+
+var myName = "Minoar"
+if (myName ==="Minoar"){
+    console.log('You are allowed');
+}
