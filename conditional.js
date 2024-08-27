6a8f6289a97bdb4e1b107df9442b7af6f54aa75b
Minoar98 <minoarhasan@gmail.com>
2024-08-27 15:40:05 +0600

    else if added

---
+++ conditional.js
@@ -0,0 +1,21 @@
+var whatDate = 'Sunday';
+if(whatDate === 'Friday'){
+    console.log('It is close today');
+} else if(whatDate === 'Sturday'){
+    console.log('It is close Today')
+}else if(whatDate === 'Thursday'){
+    console.log('It is partialy closed')
+} else{
+    console.log('It is normal working day!')
+}
+
+var whatDate = 'Friday';
+if(whatDate === 'Friday'){
+    console.log('It is close today');
+} else if(whatDate === 'Sturday'){
+    console.log('It is close Today')
+}else if(whatDate === 'Thursday'){
+    console.log('It is partialy closed')
+} else{
+    console.log('It is normal working day!')
+}
